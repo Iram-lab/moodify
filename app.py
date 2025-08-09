d2f6f0d3307bb4e9b6d180687b860681fd5a6dea
Iram_lab <irambasade-inft@atharvacoe.ac.in>
2025-08-09 08:28:18 -0400

    changes in port

--- app.py
+++ app.py
@@ -181,5 +181,6 @@ def join():
     
 """ if __name__ == "__main__":
     app.run(debug=True) """
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default for local
+    app.run(host="0.0.0.0", port=port)
